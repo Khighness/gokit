1a16d8532224152ec603661252003147bb2ff73f
cantai <cantai@pinduoduo.com>
2023-11-04 15:19:26 +0800

    Update MinHEAP unit test

--- heap/minheap_test.go
+++ heap/minheap_test.go
@@ -1,6 +1,7 @@
 package heap
 
 import (
+	"github.com/stretchr/testify/assert"
 	"strconv"
 	"testing"
 )
@@ -8,16 +9,33 @@ import (
 // @Author KHighness
 // @Update 2023-06-18
 
-func TestMinHeap_Add(t *testing.T) {
-	h := NewMinHeap(3)
-	for i := 0; i <= 100; i++ {
+func TestMinHeap_Api(t *testing.T) {
+	k := 3
+	l := 100
+	h := initMinHeap(uint32(k))
+
+	for i := k; i <= l; i++ {
 		h.Add(&Node{
 			Key: strconv.Itoa(i),
 			Val: uint32(i),
 		})
 
-		t.Logf("Add %d, Min: %d", i, h.Min())
+		assert.Equal(t, uint32(i-2), h.Min())
+		assert.Equal(t, k, h.Len())
 	}
 
-	t.Log(1 << 9)
+	for i := k; i > 0; i-- {
+		assert.Equal(t, h.Pop().Val, uint32(l-i+1))
+	}
+}
+
+func initMinHeap(k uint32) MinHeap {
+	h := NewMinHeap(k)
+	for i := 0; i < int(k); i++ {
+		h.Add(&Node{
+			Key: strconv.Itoa(i),
+			Val: uint32(i),
+		})
+	}
+	return h
 }
